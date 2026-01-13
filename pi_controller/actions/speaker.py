dc0534ef10263e83cf319f9d1279659139274a4b
Shivacodehub <poojarinagashiva123@gmail.com>
2026-01-13 10:54:04 +0530

    Update speaker.py

--- pi_controller/actions/speaker.py
+++ pi_controller/actions/speaker.py
@@ -1,1 +1,13 @@
+import pyttsx3
 
+engine = pyttsx3.init()
+
+def speak(text):
+    """
+    SOFTWARE TEAM:
+    This text comes from LLM output.
+    Do not change hardware logic.
+    """
+    if text:
+        engine.say(text)
+        engine.runAndWait()
